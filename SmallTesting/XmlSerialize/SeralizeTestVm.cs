35f8767bf98a49c537e8994bd0f94e4175086284
Mick Radtke <mick.radtke@gmail.com>
2018-10-19 14:06:39 -0500

    have grouping working now need custom views and viewmodels

--- SmallTesting/XmlSerialize/SeralizeTestVm.cs
+++ SmallTesting/XmlSerialize/SeralizeTestVm.cs
@@ -17,6 +17,8 @@ namespace SmallTesting.XmlSerialize
             GroupCommand = new ActionCommand(GroupParts);
             CreateData();
             SetGroupObjects();
+            CreatePartData();
+            GroupParts();
         }
         public ICommand GroupCommand { get; set; }
         void CreateData()
@@ -27,19 +29,26 @@ namespace SmallTesting.XmlSerialize
 
 
             GroupRootObject = new PartGroupObject();
-            var partGroup = new PartGroupObject();
+            var partTypeGroup = new PartTypeGroupObject();
+            var partNameGroup = new PartGroupObject();
             var partFilter = new PartAttributeFilterObject();
 
             GroupRootObject.GroupObjects = new List<GroupObject<Part>>();
 
-            partGroup.FilterObjects = new List<FilterObject<Part>>();
-            partGroup.GroupObjects = new List<GroupObject<Part>>();
-            partGroup.FilterObjects.Add(partFilter);
+            var partNameFilter = new PartPropertyFilterObject();
+            partNameFilter.PropertyName = "Part Name";
+            partNameFilter.Value = "Grooved Board";
+            partNameGroup.FilterObjects.Add(partNameFilter);
+
+            // partGroup.FilterObjects = new List<FilterObject<Part>>();
+            // partGroup.GroupObjects = new List<GroupObject<Part>>();
+            //partGroup.FilterObjects.Add(partFilter);
 
 
-            GroupRootObject.GroupObjects.Add(partGroup);
+            GroupRootObject.GroupObjects.Add(partTypeGroup);
+            GroupRootObject.GroupObjects.Add(partNameGroup);
 
-            XmlSerializer xsSubmit = new XmlSerializer(GroupRootObject.GetType(), new Type[] { typeof(PartGroupObject), typeof(PartAttributeFilterObject) });
+            XmlSerializer xsSubmit = new XmlSerializer(GroupRootObject.GetType(), new Type[] { typeof(PartTypeGroupObject), typeof(PartGroupObject), typeof(PartPropertyFilterObject), typeof(PartAttributeFilterObject) });
 
             using (var sww = new StringWriter())
             {
@@ -60,9 +69,9 @@ namespace SmallTesting.XmlSerialize
         void SetGroupObjects()
         {
             GroupObjects = new List<GroupObject<Part>>();
-            GroupObjects.Add(new AttributeGroupObject());
+            //GroupObjects.Add(new AttributeGroupObject());
             GroupObjects.Add(new PartTypeGroupObject());
-            GroupObjects.Add(new PartGroupObject());
+            //GroupObjects.Add(new PartGroupObject());
         }
 
         void CreatePartData()
@@ -79,7 +88,7 @@ namespace SmallTesting.XmlSerialize
                 new Part { Name = "",Length = 27,Type = "2x8"},
                 new Part { Name = "",Length = 96,Type = "6x8"}
             };
-             
+
         }
 
         void GroupParts()
@@ -87,11 +96,9 @@ namespace SmallTesting.XmlSerialize
             var itemGroup = new ItemGroup<Part>();
             itemGroup.Items = Parts;
 
-            GroupedParts = GroupRootObject.Group(new List<ItemGroup<Part>> { itemGroup });
-
-        }
-
-
+            GroupedParts = GroupObject<Part>.CreateGroupItems(Parts);
+            GroupRootObject.Group(GroupedParts);
+            }
         void GetGroupObjectsFromXml()
         {
 
@@ -101,136 +108,132 @@ namespace SmallTesting.XmlSerialize
         public List<GroupObject<Part>> GroupObjects { get; set; }
         public bool SameString { get; set; }
         public List<Part> Parts { get; set; }
-        public List<ItemGroup<Part>> GroupedParts { get; set; }
-
+        public List<GroupItem<Part>> GroupedParts { get; set; }
     }
     public class GroupObject<T>
     {
         public GroupObject()
         {
+            GroupObjects = new List<GroupObject<T>>();
+            FilterObjects = new List<FilterObject<T>>();
         }
-
-        public List<ItemGroup<T>> ItemGroups { get; set; }
+        //public List<GroupItem<T>> GroupItems { get; set; }
 
         public int Id { get; set; }
         public List<FilterObject<T>> FilterObjects { get; set; }
         public List<GroupObject<T>> GroupObjects { get; set; }
         public string Name { get; set; }
-        public virtual List<ItemGroup<T>> Group(List<ItemGroup<T>> groupObjects, int index = 0)
+        public virtual void Group(List<GroupItem<T>> groupItems)
         {
-            Id = ++index;
             var newGroupObjects = new List<ItemGroup<T>>();
             foreach (var group in GroupObjects)
-                group.Group(ItemGroups);
-            return newGroupObjects;
+                group.Group(groupItems);
         }
 
-        ItemGroup<T> GetItemGroup(List<int> ancestry)
+        public virtual List<GroupItem<T>> Filter(List<GroupItem<T>> groupItems)
         {
-            foreach(var itemGroup in ItemGroups)
+            return null;
+        }
+
+        public static List<GroupItem<T>> CreateGroupItems(List<T> items)
+        {
+            var groupItems = new List<GroupItem<T>>();
+            foreach (var item in items)
             {
-                if (itemGroup.Ancestry.SequenceEqual(ancestry))
-                    return itemGroup;
+                var groupItem = new GroupItem<T>();
+                groupItem.Item = item;
+                groupItems.Add(groupItem);
             }
-            return null;
-        }
-        public virtual List<ItemGroup<T>> Filter(List<ItemGroup<T>> itemGroups)
-        {
-            return null;
-        }
 
-
+            return groupItems;
+        }
         //public Func<List<object>, List<object>> Group { get; set; }
     }
     public class ItemGroup<T>
     {
-        public List<int> Ancestry { get; set; }
+        public ItemGroup()
+        {
+            Items = new List<T>();
+        }
         public string Name { get; set; }
         public List<T> Items { get; set; }
+    }
+    public class GroupItem<T>
+    {
+        public GroupItem()
+        {
+            GroupNames = new List<string>();
+        }
+        public string Name { get; set; }
+        public T Item { get; set; }
+        public List<string> GroupNames { get; set; }
     }
     public class FilterObject<T>
     {
         public string Name { get; set; }
-        public virtual List<ItemGroup<T>> Filter(List<ItemGroup<T>> itemGroups)
+        public virtual List<GroupItem<Part>> Filter(List<GroupItem<Part>> groupItems)
         {
             return null;
         }
         //public Func<List<object>, List<object>> Filter { get; set; }
     }
-
     public class PartAttributeFilterObject : FilterObject<Part>
     {
         public PartAttributeFilterObject()
         {
             //Filter = FilterAttributes;
         }
-        
     }
-
     public class PartPropertyFilterObject : FilterObject<Part>
     {
         public PartPropertyFilterObject()
         {
             Name = "Part Property";
             PropertyNames = new List<string> { "Part Type", "Part Name" };
-
         }
 
         public List<string> PropertyNames { get; set; }
         public string Value { get; set; }
         public string PropertyName { get; set; }
-        
-        public override List<ItemGroup<Part>> Filter(List<ItemGroup<Part>> itemGroups)
+
+        public override List<GroupItem<Part>> Filter(List<GroupItem<Part>> groupItems)
         {
+            Name = Value;
             switch (PropertyName)
             {
                 case "Part Type":
-                    return FilterByPartType(itemGroups);
+                    return FilterByPartType(groupItems);
                 case "Part Name":
-                    return FilterByPartName(itemGroups);
+                    return FilterByPartName(groupItems);
                 default:
-                    return new List<ItemGroup<Part>>(); 
+                    return new List<GroupItem<Part>>();
             }
         }
 
-        List<ItemGroup<Part>> FilterByPartType(List<ItemGroup<Part>> itemGroups)
+        List<GroupItem<Part>> FilterByPartType(List<GroupItem<Part>> items)
         {
-            var newItemGroups = new List<ItemGroup<Part>>();
+            var newGroupItems = new List<GroupItem<Part>>();
 
-            foreach (var itemGroup in itemGroups)
+            foreach (var item in items)
             {
-                var newGroup = new ItemGroup<Part>();
-                newGroup.Items = new List<Part>();
-                foreach (var item in itemGroup.Items)
-                {
-                    if (item.Type == Value)
-                        newGroup.Items.Add(item);
-                }
-                newItemGroups.Add(newGroup);
+                if (item.Item.Type == Value)
+                    newGroupItems.Add(item);
+
             }
-            return newItemGroups;
+            return newGroupItems;
         }
 
-        List<ItemGroup<Part>> FilterByPartName(List<ItemGroup<Part>> itemGroups)
+        List<GroupItem<Part>> FilterByPartName(List<GroupItem<Part>> items)
         {
-            var newItemGroups = new List<ItemGroup<Part>>();
+            var newGroupItems = new List<GroupItem<Part>>();
 
-            foreach (var itemGroup in itemGroups)
+            foreach (var item in items)
             {
-                var newGroup = new ItemGroup<Part>();
-                newGroup.Items = new List<Part>();
-                foreach (var item in itemGroup.Items)
-                {
-                    if (item.Name == Value)
-                        newGroup.Items.Add(item);
-                }
-                newItemGroups.Add(newGroup);
+                if (item.Item.Name == Value)
+                    newGroupItems.Add(item);
             }
-            return newItemGroups;
+            return newGroupItems;
         }
-
-
-
     }
 
     public class PartGroupObject : GroupObject<Part>
@@ -238,6 +241,32 @@ namespace SmallTesting.XmlSerialize
         public PartGroupObject()
         {
             Name = "Part";
+        }
+
+        public override void Group(List<GroupItem<Part>> groupItems)
+        {
+            string Name = "";
+            string space = "";
+
+            foreach (var filter in FilterObjects)
+            {
+                Name += space;
+                groupItems = filter.Filter(groupItems);
+                Name += filter.Name;
+                space = " ";
+            }
+
+            if (!string.IsNullOrWhiteSpace(Name))
+                foreach (var item in groupItems)
+                {
+                    item.GroupNames.Add(Name);
+                }
+
+            base.Group(groupItems);
+        }
+        public override List<GroupItem<Part>> Filter(List<GroupItem<Part>> groupItems)
+        {
+            return base.Filter(groupItems);
         }
     }
 
@@ -248,20 +277,12 @@ namespace SmallTesting.XmlSerialize
             Name = "Part Type";
         }
 
-        public override List<ItemGroup<Part>> Group(List<ItemGroup<Part>> itemGroups, int index = 0)
+        public override void Group(List<GroupItem<Part>> groupItems)
         {
-
-            foreach (var itemGroup in itemGroups)
+            foreach (var item in groupItems)
             {
-                var newItemGroup = new ItemGroup<Part>();
-
-                foreach (var part in itemGroup.Items)
-                {
-                    if (groupObject != null)
-                        groupObject.Items.Add(part);
-                }
+                item.GroupNames.Add(item.Item.Type);//just add the part type name as the groupname
             }
-
         }
     }
 
@@ -274,5 +295,7 @@ namespace SmallTesting.XmlSerialize
             FilterObjects.Add(new PartAttributeFilterObject());
         }
     }
+
+  
 
 }
